98debf4c2c8b8948455cdadd3e115d9fb60f772d
Nakamury1 <sophielamsova29@gmail.com>
2025-03-17 09:51:14 +0100

    fix: update connexion page

--- Cantine_Ecole/Traitement_connexion.php
+++ Cantine_Ecole/Traitement_connexion.php
@@ -9,13 +9,6 @@ if (!isset($_POST['token']) || $_POST['token'] != $_SESSION['csrf_connexion_add'
 // Supprime le token en session pour qu'il soit regénéré
 unset($_SESSION['csrf_connexion_add']);
 
-if (isset($_POST["pseudo"]) && !empty($_POST["pseudo"])){
-    $pseudo = htmlspecialchars($_POST["pseudo"]);
-}
-else {
-    echo "<p>Le pseudo est obligatoire</p>";
-}
-
 if (isset($_POST["mail"]) && !empty($_POST["mail"])){
     $mail = htmlspecialchars($_POST["mail"]);
 }
@@ -36,25 +29,24 @@ else {
 require_once 'bdd.php';
 
 // Préparation de la requête
-$sql = "SELECT pseudo, mail, mdp FROM connexion WHERE pseudo = :pseudo AND mail = :mail AND mdp = :mdp";
+$sql = "SELECT mail, mdp FROM connexion WHERE mail = :mail AND mdp = :mdp";
 $req = $connexion->prepare($sql);
-$req->bindParam(':pseudo', $pseudo);
 $req->bindParam(':mail', $mail);
 $req->bindParam(':mdp', $mdp);
 $req->execute();
 
 // Vérification des résultats
 if ($rep = $req->fetch()) {
-    if (($pseudo == ($rep['pseudo']='admin')) && ($mail == ($rep['mail']='admin')) && ($mdp == ($rep['mdp']='admin'))){
-        echo "<p>Connexion réussie ! <a href='Admin_Liste_plantes.php'>Accéder à la liste de toutes les plantes pour les admins</a></p>";
+    if (($mail == ($rep['mail']='admin')) && ($mdp == ($rep['mdp']='admin'))){
+        echo "<p>Connexion réussie ! <a href='./Admin_Liste_Menu.php'>Accéder à la liste de toutes les menus pour les admins</a></p>";
     }
     else{
-        echo "<p>Connexion réussie ! <a href='User_Liste_plantes.php'>Accéder à la liste de toutes les plantes</a></p>";
+        echo "<p>Connexion réussie ! <a href='./User_Liste_Menu.php'>Accéder à la liste de toutes les menus</a></p>";
     }
 }
 else{
-    echo "Le pseudo, l'adresse mail et le mot de passe ne correspondent pas";
-    echo "<p><a href='HTML_Inscription.php'>inscrivez-vous !</a></p>";
+    echo "L'adresse mail et le mot de passe ne correspondent pas";
+    echo "<p><a href='./HTML_Inscription.php'>inscrivez-vous !</a></p>";
 }
 
 ?>
