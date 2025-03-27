b6f1480be38dd8a4a96271a80846e46912668d01
Nakamury1 <sophielamsova29@gmail.com>
2025-03-27 09:26:07 +0100

    delete files

--- test_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $connexion = new PDO("mysql:host=localhost;dbname=BAP2_equipe3", "root", "");
-    echo "Connexion réussie à la base de données.";
-} catch (PDOException $e) {
-    die("Erreur SQL : " . $e->getMessage());
-}
